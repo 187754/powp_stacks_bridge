48254c818d08c3a9bac5f730f352e36fead94d57
187754 <krystianleszczynski1994@o2.pl>
2017-03-02 15:36:28 +0100

    punkt 3.1.8.

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -2,11 +2,37 @@ package edu.kis.vh.stacks.list;
 
 public class Node {
 
-	public int value;
-	public Node prev, next;
+	private int value; // wszystkie użycia zmiennej zostały zmienione na użycia
+						// poprzez getter i setter (klasa StackList)
+	private Node prev;
+	private Node next;// tak samo jak w przypadku value
 
 	public Node(int i) {
 		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
 	}
 
 }
