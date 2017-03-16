d83966af3725b607113f0b84400007e78b36409a
187754 <krystianleszczynski1994@o2.pl>
2017-03-16 14:43:21 +0100

    lab2. 3.1.1. i 3.1.2.

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -5,90 +5,43 @@ package edu.kis.vh.stacks;
  *
  */
 public class Stack {
+	
+	public Stack(StackArray stackArray) {
+		super();
+		this.stackArray = stackArray;
+	}
 
-	/**
-	 * ilość elementów pełnego stosu
-	 * 
-	 */
-	private static final int FULL_STACK_INDICATOR = 11;
-
-	/**
-	 * wartość zwracana, gdy stos jest pusty
-	 * 
-	 */
-	private static final int EMPTY_STACK = -1;
-
-	/**
-	 * tablica przechowująca elementy stosu
-	 * 
-	 */
-	private final int[] items = new int[FULL_STACK_INDICATOR + 1];
-
-	/**
-	 * wskaźnik stosu
-	 */
-	private int total = EMPTY_STACK;
-
-	/**
-	 * @return zwraca ilość elementów, które znajdują się na stosie
-	 */
+	public Stack(){
+		this.stackArray = new StackArray();
+	}
+	private StackArray stackArray;
 	public int getTotal() {
-		return total;
+		return stackArray.getTotal();
 	}
 
-	/**
-	 * dokłada element na stos
-	 * 
-	 * @param i
-	 *            wartość elementu, który ma zostać odłożony na stos
-	 */
 	public void push(int i) {
-		if (!isFull())
-			items[++total] = i;
+		stackArray.push(i);
 	}
 
-	/**
-	 * sprawdza czy stos jest pusty
-	 * 
-	 * @return true - gdy stos jest pusty, false w przeciwnym wypadku
-	 */
 	public boolean isEmpty() {
-		return total == EMPTY_STACK;
+		return stackArray.isEmpty();
 	}
 
-	/**
-	 * sprawdza czy stos jest pełen
-	 * 
-	 * @return true gdy stos jest pełen, false w przeciwnym wypadku
-	 */
 	public boolean isFull() {
-		return total == FULL_STACK_INDICATOR;
+		return stackArray.isFull();
 	}
 
-	/**
-	 * zwraca wartość z wierzchołka stosu bez usuwania jej ze stosu
-	 * 
-	 * @return EMPTY_STACK gdy stos jest pusty, w przeciwnym wypadku wartość
-	 *         elementu ze szczytu stosu
-	 *
-	 */
 	public int top() {
-		if (isEmpty())
-			return EMPTY_STACK;
-		return items[total];
+		return stackArray.top();
 	}
 
-	/**
-	 * pobiera wartość ze stosu, usuwając ją z niego równocześnie
-	 * 
-	 * @return EMPTY_STACK gdy stos jest pusty, w przeciwnym wypadku wartość
-	 *         elementu ze szczytu stosu
-	 * 
-	 */
 	public int pop() {
-		if (isEmpty())
-			return EMPTY_STACK;
-		return items[total--];
+		return stackArray.pop();
 	}
+
+
+
+
+	
 
 }
